cea27b999d58905c4dbec20d2611daa3883f83e2
Vitor riquelme <vitoriquelme1@gmail.com>
2022-06-29 20:49:43 -0300

    exercicios usando javascript

---
+++ aula012/ex001.js
@@ -0,0 +1,8 @@
+var pais = "Mongolia"
+console.log(`vivendo em ${pais}`)
+if(pais=="Brasil"){
+    console.log("você é Brasileiro")
+}
+else{
+    console.log("voce é Gringo")
+}
--- aula012/index.js
+++ aula012/index.js
@@ -1,3 +1,9 @@
 
-console.log("o console funcione corretamente")
+var vel = 55
+console.log("o maximo de velocidade permitida nesta estrada é de 60km/h")
+console.log(`a velocidade do seu veiculo é de ${vel} km/h`)
+if(vel > 60) {
+    console.log("você ultrapassou a velocidade permitida MULTADO")
+}
+console.log("dirija sempre usando sempre cinto de segurança")
 
