043f1924ffc7a596b291d6169cf3cdbf594c4aae
Vitor riquelme <vitoriquelme1@gmail.com>
2022-07-13 18:52:19 -0300

    excercicios de javascript

---
+++ aula027/index.js
@@ -0,0 +1,7 @@
+var pontos = 60
+var energia = 70
+if(pontos >=30 && pontos <=40 && energia >=60 || pontos>=60 && pontos<=70 && energia<=60){
+    console.log(" você ganhou !!!")
+}else{
+    console.log("você perdeu")
+}
---
+++ aula028/index.js
@@ -0,0 +1,13 @@
+var notas = 40
+if(notas>=95){
+    console.log("aprovado com louvor")
+}
+else if(notas >=60){
+    console.log("você foi aprovado")
+}else if(notas >=50){
+    console.log("você está de recuperção")
+}else if(notas>=20){
+    console.log("você foi reprovado")
+}else{
+    console.log("reprovado com louvor")
+}
