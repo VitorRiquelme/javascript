c9c624dfe2cf89e11486bb9f7d16778c7253c3fb
Vitor riquelme <vitoriquelme1@gmail.com>
2022-07-12 23:07:28 -0300

    exercicios de javscript

---
+++ aula024/index.js
@@ -0,0 +1,4 @@
+let canal = "cfb cursos"
+let num = 2018
+console.log(canal)
+console.log(num)
---
+++ aula025/index.js
@@ -0,0 +1,9 @@
+var nota = 50
+
+if(nota >= 60){
+    console.log("Aprovado")
+}else if (nota >=40){
+    console.log("Recuperação")
+}else{
+    console.log("reprovado")
+}
