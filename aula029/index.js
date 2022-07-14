a0536a29542c84fc6dbcf7a17b0d28e6c706ec15
Vitor riquelme <vitoriquelme1@gmail.com>
2022-07-13 21:12:56 -0300

    excercicio de javascript

---
+++ aula029/index.js
@@ -0,0 +1,18 @@
+var pos
+pos=2
+let msg
+
+switch(pos){
+    case 1:
+        msg:"primeiro lugar <br> medalha de ouro"
+        break
+    case 2:
+         msg:"Segundo lugar <br> medalha de prata"
+         break
+    case 3:
+        msg:"Terceiro lugar <br> medalha de bronze"
+        break
+    default:
+        msg:"Não subiu ao podio"          
+}
+console.log(msg)
