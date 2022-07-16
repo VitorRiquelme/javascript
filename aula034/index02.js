90498319418c9173cd106f6ad2ea61b64a367e09
Vitor riquelme <vitoriquelme1@gmail.com>
2022-07-15 22:55:04 -0300

    excercicios de funções em javascript

---
+++ aula034/index02.js
@@ -0,0 +1,5 @@
+let dt=new Date()
+let dia=dt.getDay
+let mes=dt.getMonth
+let ano=dt.getFullYear
+console.log(dt)
---
+++ aula035/index.js
@@ -0,0 +1,22 @@
+//function escrever(){
+   // console.log("iae rapaziada")
+//}
+
+//escrever()
+
+/*function soma(n1, n2){
+    let res=n1+n2
+    console.log(res)
+}
+soma(5,2)
+soma(10,25)
+soma(6,19)*/
+/*function soma(n1, n2){
+    let res =n1+n2
+    return res
+}
+console.log(soma(1,2))*/
+
+function media(nums){
+
+}
