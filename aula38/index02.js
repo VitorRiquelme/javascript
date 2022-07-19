e16bb10a8733fdfe6eadbd1451d864a4b0e44642
Vitor riquelme <vitoriquelme1@gmail.com>
2022-07-19 17:43:03 -0300

    excercios de javascript

---
+++ aula38/index02.js
@@ -0,0 +1,10 @@
+let nome =45
+
+if(nome<=12){
+    console.log("voce é criança")
+}else if(nome<=16){
+    console.log("você é adolescente")
+}else if(nome<=40){
+    console.log("você é adulto")
+}else{
+    console.log("voce é vei")}
---
+++ aula38/index03.js
@@ -0,0 +1,8 @@
+
+function somar(x,y){
+     let res = x+y
+     console.log(res)
+}
+somar(5,2)
+somar(8,10)
+somar(10,1212)
