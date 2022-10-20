83da1bc0de3fe8b8bd27eb33bb41282f3cccb3e6
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-20 18:40:49 -0300

    Operadores de comparação javascript

---
+++ ex06/index.js
@@ -0,0 +1,9 @@
+//Operadores de comparação (condicionais)
+// igual (==) - verifica se os valores comparados são iguais
+// idêntico(===) - verifica se os valores são iguais e do mesmo tipo
+// diferente(!=) - verifica se os valores comparados são diferentes
+// não idêntico (!==) - verifica se os valores comparados são diferentes e de diferentes tipos
+// Menor (<) - verifica se o valor da esquerda é menor que o valor da direita
+// Maior (>) - verifica se o valor da esquerda é maior que o valor da direita
+// Menor igual (<=) verifica se o valor da esquerda é menor ou igual o valor da direita
+// Maior igual (=>) verifica se o valor da esquerda é maior ou igual o valor da direita
