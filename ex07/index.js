5584386a85288f5692f7b2eddb1848b4ee180ef0
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-20 19:37:21 -0300

    ToString javascript

--- ex07/index.js
+++ ex07/index.js
@@ -1,12 +1,13 @@
-var variavel1 = prompt('Digite algum numero: ')
-var variavel2 = prompt('Digite outro numero: ')
+var variavel1 = 10
+var variavel2 = 20
 
 //ParseInt para converte uma string em um número inteiro
 /*variavel1 = parseInt(variavel1)
 variavel2 = parseInt(variavel2)*/
 
 //Parse float preserva a fração (caso tenho) do valor nùmerico enquanto o parseInt retira essa fração
-variavel1 = parseFloat(variavel1)
-variavel2 = parseFloat(variavel2)
+//variavel1 = parseFloat(variavel1)
+//variavel2 = parseFloat(variavel2)
 
-document.write(variavel1 + variavel2)
+//ToString transforma o valor em uma string
+document.write(variavel1.toString() + variavel2.toString())
