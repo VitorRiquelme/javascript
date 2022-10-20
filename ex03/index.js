fdc4d838ac75915bed1d9a9bdb5a131d87051fbf
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-19 22:35:29 -0300

    variavel prompt e contatenação

--- ex03/index.js
+++ ex03/index.js
@@ -1,3 +1,3 @@
-var nome = prompt('dgite seu nome filha da puta')
+var nome = prompt('dgite seu nome filha da puta')//´Prompt é usado para voce dizer para o usario o que ele quer escrever o que é escrito no prompt passa a ser o valor atribuado a variavel no caso ali nome//
 var idade = prompt('digite sua idade agora arrombado')
 document.write('<h1>ola ' + nome +' vejo que voce tem ' + idade +' anos </h1>' )
