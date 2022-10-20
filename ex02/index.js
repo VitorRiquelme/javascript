410bda7d9452f132fbd2c9824394fda038281a5c
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-19 22:25:23 -0300

    variaves em javascript

--- ex02/index.js
+++ ex02/index.js
@@ -1,2 +1,13 @@
-//Variavel do tipo string:
-var 
+//Variavel do tipo string: usando sempre ' ou " " para uma string
+var texto = 'Curso de javascript'
+//number não usa aspa
+var numerointeiro = -7 //int
+var numerofracionario = 10.75 //float
+//bolean n]ao usa aspa
+var teste = true
+// alert abre um dialog
+alert()
+//document.write
+document.write(texto)
+//console.lgo
+console.log(texto)
