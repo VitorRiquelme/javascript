c25445cfbcc63fe1998fc748cc6fb173375935ef
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-19 22:33:41 -0300

    Javascript contatenação e prompt

---
+++ ex03/index.js
@@ -0,0 +1,3 @@
+var nome = prompt('dgite seu nome filha da puta')
+var idade = prompt('digite sua idade agora arrombado')
+document.write('<h1>ola ' + nome +' vejo que voce tem ' + idade +' anos </h1>' )
