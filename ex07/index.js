f38d13688e8df1bde1ef6470b296a9c7ccf2f1f7
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-20 19:33:40 -0300

    ParseFloat

--- ex07/index.js
+++ ex07/index.js
@@ -1,7 +1,12 @@
 var variavel1 = prompt('Digite algum numero: ')
 var variavel2 = prompt('Digite outro numero: ')
+
 //ParseInt para converte uma string em um número inteiro
-variavel1 = parseInt(variavel1)
-variavel2 = parseInt(variavel2)
+/*variavel1 = parseInt(variavel1)
+variavel2 = parseInt(variavel2)*/
+
+//Parse float preserva a fração (caso tenho) do valor nùmerico enquanto o parseInt retira essa fração
+variavel1 = parseFloat(variavel1)
+variavel2 = parseFloat(variavel2)
 
 document.write(variavel1 + variavel2)
