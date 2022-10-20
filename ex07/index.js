98125d46da08f56fe78b835e5ebd73278ee5c8b7
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-20 19:30:52 -0300

    ParseInt javascript

---
+++ ex07/index.js
@@ -0,0 +1,7 @@
+var variavel1 = prompt('Digite algum numero: ')
+var variavel2 = prompt('Digite outro numero: ')
+//ParseInt para converte uma string em um número inteiro
+variavel1 = parseInt(variavel1)
+variavel2 = parseInt(variavel2)
+
+document.write(variavel1 + variavel2)
