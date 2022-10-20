4d3d5fd96f28908cf172da1e79aad3e3a8b23b1d
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-19 22:38:10 -0300

    Variaves em javascritp

--- ex03/index.js
+++ ex03/index.js
@@ -1,3 +1,6 @@
 var nome = prompt('dgite seu nome filha da puta')//´Prompt é usado para voce dizer para o usario o que ele quer escrever o que é escrito no prompt passa a ser o valor atribuado a variavel no caso ali nome//
-var idade = prompt('digite sua idade agora arrombado')
+var idade = prompt('digite sua idade agora arrombado')//Prompt sempre recebera os valores como string
+var idade2 = 52
 document.write('<h1>ola ' + nome +' vejo que voce tem ' + idade +' anos </h1>' )
+console.log(idade)
+console.log(idade2)
