ef7057c3fc3aa091272713fa0a3e57a203cf5128
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-20 19:41:04 -0300

    Operadores logicos

---
+++ ex08/index.js
@@ -0,0 +1,4 @@
+//Operadores logicos:
+//E (&&) -  Verdadeiro se todas as expressões forem verdadeiras
+//Ou  (II)- verdadeiro se pelo menos uma das expressões forem verdaedeiras
+//Negação (!) - inverte o resultado da expressão de comparação
