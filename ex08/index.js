3cf0600080216b880c768a04fabfe804ed74ee13
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-20 20:01:03 -0300

    Operadores lógicos

--- ex08/index.js
+++ ex08/index.js
@@ -1,4 +1,19 @@
-//Operadores logicos:
+//Operadores lógicos:
 //E (&&) -  Verdadeiro se todas as expressões forem verdadeiras
-//Ou  (II)- verdadeiro se pelo menos uma das expressões forem verdaedeiras
+//Ou  (||)- verdadeiro se pelo menos uma das expressões forem verdaedeiras
 //Negação (!) - inverte o resultado da expressão de comparação
+if(2==2 && 3>=1 && 'a' == 'b'){//&& precisa que TODAS as expressões sejam verdadeiras para retornar true
+  document.write('igual')
+}else(
+  document.write('nada haver ')
+)
+if(3==3 || 1>=2 || 'q' == 'b'){ // || só precisa que uma das expressões sejam verdadeiras para retornar true
+  document.write('certissimo irmao')
+}else(
+  document.write('que isso nada haver')
+)
+if(!(1>2)){//!() Inverte o valor da expressão para false caso seja true e vise e versa
+  document.write('sou gay')
+}else(
+  document.write('iAE')
+)
