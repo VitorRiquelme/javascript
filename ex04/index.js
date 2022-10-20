2c9c9dd0fb662be597dafd9dcf51e8170a45c70d
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-19 22:54:58 -0300

    alternando o valor da variavel

---
+++ ex04/index.js
@@ -0,0 +1,13 @@
+//null siginifica a ausensia intecional de um valor
+  var test = null
+//undefined signifca que a valor ainda não foi defenido se deixar a variavel com nada escrito dentro também vamos obter o mesmo resultado//
+  var mlk = undefined
+
+  console.log(test)
+  console.log(mlk)
+
+  test = 'iae maninho aqui quem fala é o ycaro'
+  console.log(test)
+
+  test = 1120
+  console.log(test)
