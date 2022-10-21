0474d15f5fe281ba3515012edc1bc0072d5fd570
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-20 21:16:21 -0300

    excericicos de fixação usando if e else

---
+++ ex09/index.js
@@ -0,0 +1,16 @@
+var idade = prompt('digite sua idade')
+
+
+
+if(idade>=0 && idade <=15){
+  document.write('Criança kkk')
+}
+if(idade>=15 && idade<=30){
+  document.write('jovem')
+}
+if(idade>=30 && idade<=60){
+  document.write('adulto')
+}
+if(idade>=60){
+  document.write('Idoso')
+}
