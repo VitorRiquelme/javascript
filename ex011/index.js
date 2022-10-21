97c5092bdf1fc29720c746868156f1b372a7cec9
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-21 20:58:07 -0300

    Operadores aritimeticos

--- ex011/index.js
+++ ex011/index.js
@@ -1,1 +1,8 @@
 //Operadores aritemeticos
+//Adição(+) - somar valores
+//Subtração(-) - diferença entre valores
+//Multiplicação(*) - Multiplica os valores
+//Divisão(/) - Divide os valores
+//Módulo(%) - Resto existente de uma operação de divisão
+//(++) - Pré/pós incremento
+//(--) - Pré/pós decremento
