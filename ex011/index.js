372320011565946ac69be5713cee21f2d6c42aae
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-24 17:02:18 -0300

    Praticando com operadores aritimeticos

--- ex011/index.js
+++ ex011/index.js
@@ -6,3 +6,10 @@
 //Módulo(%) - Resto existente de uma operação de divisão
 //(++) - Pré/pós incremento
 //(--) - Pré/pós decremento
+var num = 2
+var num2 = 9
+
+document.write('A soma entre os valores ' + num + ' e ' + num2 + ' é: '  + (num+num2) + '<br>')
+document.write('A subtração entre ' + num + ' e ' + num2 + ' é: '+ (num2-num) + '<br>')
+document.write('A multiplicação entre ' + num + ' e ' + num2 + ' é: ' + (num*num2) + '<br>')
+document.write('A divisão entre os valores ' + num + ' e ' + num2 + ' é: '+ (num/num2))
