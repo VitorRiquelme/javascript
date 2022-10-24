f0775f68223eb9dd2df5aae2e7897ce4623a5a6e
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-24 17:24:25 -0300

    operadores aritimeticos

--- ex011/index.js
+++ ex011/index.js
@@ -6,7 +6,8 @@
 //Módulo(%) - Resto existente de uma operação de divisão
 //(++) - Pré/pós incremento
 //(--) - Pré/pós decremento
-var num = prompt('digite o valor de num')
+
+/*var num = prompt('digite o valor de num')
 var num2 = prompt('Digite o valor de num2')
 
 num = parseInt(num)
@@ -16,4 +17,15 @@ document.write('A soma entre os valores ' + num + ' e ' + num2 + ' é: '  + (num
 document.write('A subtração entre ' + num + ' e ' + num2 + ' é: '+ (num2-num) + '<br>')
 document.write('A multiplicação entre ' + num + ' e ' + num2 + ' é: ' + (num*num2) + '<br>')
 document.write('A divisão entre os valores ' + num2 + ' e ' + num + ' é: '+ (num2/num) + '<br>')
-document.write('O modulo entre' + num + ' e ' + num2 + ' é: ' + (num2%num) + '<br>')
+document.write('O modulo entre' + num + ' e ' + num2 + ' é: ' + (num2%num) + '<br>')*/
+
+/*var teste = 10
+//teste +=5
+//teste -=5
+//teste *=5
+teste /= 5
+document.write(teste)*/
+
+document.write(11 + 5 - 5 * 2 / 2)
+// 1º Multiplicação e divisão
+// 2º Soma e subtração
