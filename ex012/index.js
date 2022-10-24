6c4bab962171da80c9e2fbd78930cfe56a9acb6e
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-24 18:22:22 -0300

    Desafio
    Um dasafio de criar um progama que de acordo com seu peso altutra diz se sua massa esta abaixo da média ou não

--- ex012/index.js
+++ ex012/index.js
@@ -1,7 +1,7 @@
 var nome = prompt('Digite seu nome')
 var altura = prompt('Digite qual a sua altura')
 var peso = prompt('Digite seu peso')
-var m = null
+var m = 
 
 altura = parseFloat(altura)
 peso = parseFloat(peso)
@@ -10,4 +10,22 @@ altura = altura/100
 
 m = peso / (altura * altura)
 
-document.write(nome+ 'Sua massa é ' + m)
+if(m <16){
+  document.write(nome + ' sua massa é ' + m + ' É um baixo peso muito grave')
+}else if(m>=16 && m<=16.99){
+  document.write(nome + ' sua masse é ' + m + ' É um baixo peso grave')
+}else if (m>=17 && m<=18.49){
+  document.write(nome + ' sua masse é ' + m + ' É um baixo peso')
+}else if(m>=18.50 && m<=24.99){
+  document.write(nome + ' sua masse é ' + m + ' É um peso normal')
+}else if(m>=25 && m<=29.99){
+  document.write(nome + ' sua masse é ' + m + ' Sobrepeso')
+}else if(m>=30 && m<=34.99){
+  document.write(nome + ' sua masse é ' + m + ' Obesidade grau 1')
+}else if(m>=35 && m<=39.99){
+  document.write(nome + ' sua masse é ' + m + ' Obesidade grau 2')
+}else if (m>=40){
+  document.write(nome + ' sua masse é ' + m + ' Obesidade grau 3')
+}
+
+
