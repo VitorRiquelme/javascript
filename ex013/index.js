3f554bacc8056dc4e696ad10e7ca4e067ecb513b
Vitor riquelme <vitoriquelme1@gmail.com>
2022-10-24 21:13:05 -0300

    funções javascript

---
+++ ex013/index.js
@@ -0,0 +1,16 @@
+
+/*function CalcularAreaTerreno (largura, comprimento){
+  var area = largura*comprimento
+  
+  return area
+}
+
+  var largura = prompt('digite a largura')
+  var comprimento = prompt('digira o comprimento')
+  var area = CalcularAreaTerreno(largura,comprimento)
+  document.write('caralho ' + area)*/
+
+  function soma (a,b){
+    return a + b
+  }
+  console.log(soma(7,2))
