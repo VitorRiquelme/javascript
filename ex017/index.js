00b36c5893a81e93bc7aef0cafc393e5b3666751
VitorRiquelme <vitoriquelme1@gmail.com>
2022-11-02 20:34:47 -0300

    Arrays, for loop

--- ex017/index.js
+++ ex017/index.js
@@ -27,12 +27,30 @@ for (let i = 0; i <= nome.length; i++) {
 
   console.log(nome[i])
   
-}*/
+}
+
 let nome = 'vitor riquelme oliveira mendes'
 for (let i = 0; i <= nome.length; i++) {
   const letras = nome[i];
   console.log(letras)
-}
+}*/
 
+let notas = []
+
+notas.push(5)
+notas.push(7)
+notas.push(8)
+notas.push(2)
+notas.push(5)
+notas.push(8)
+
+let soma = 0
+
+for (let i = 0; i < notas.length; i++) {
+  const nota = notas[i];
+  soma = soma + nota
+}
+const media = soma / notas.length
+console.log(media)
 
 
