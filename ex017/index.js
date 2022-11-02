2f8072a5b60870aca841be981dc4fe2e6c30de37
VitorRiquelme <vitoriquelme1@gmail.com>
2022-11-02 20:07:58 -0300

    arrays em javascript

--- ex017/index.js
+++ ex017/index.js
@@ -1,1 +1,15 @@
 //Arrays e estruturas de repetições em javascript
+//Push para adicionar um valor no final da lista
+//pop para pear e guarda o ultimo valor da lista
+//shift para  pegar e guarda o primeiro valor da lista
+
+let nomes = []
+
+nomes.push('renan')
+
+nomes.push(19)
+
+nomes.shift()
+
+console.log(nomes)
+
