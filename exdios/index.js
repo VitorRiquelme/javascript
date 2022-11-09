3fbef5533522d015571872f2e7b0809657635ed6
VitorRiquelme <vitoriquelme1@gmail.com>
2022-11-08 21:54:11 -0300

    desafios de codigos em javascript

---
+++ exdios/index.js
@@ -0,0 +1,10 @@
+// IMPORTANTE: As funções "gets" e "print" são acessíveis globalmente, onde:  
+// - "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário; 
+// - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente. 
+
+var a = parseInt(gets(3));
+var N = parseInt(gets(18));
+ 
+//TODO:  Retorne o valor da soma de todos múltiplos de "A" até o seu limite "N".
+
+
