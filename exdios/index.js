100dbc80465b19cb49fbdedc91bc34aea9b2464a
VitorRiquelme <vitoriquelme1@gmail.com>
2022-11-10 12:52:08 -0300

    Update index.js

--- exdios/index.js
+++
@@ -1,8 +0,0 @@
-// IMPORTANTE: As funções "gets" e "print" são acessíveis globalmente, onde:  
-// - "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário; 
-// - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente. 
-
-var a = parseInt(gets());
-var N = parseInt(gets());
- 
-//TODO:  Retorne o valor da soma de todos múltiplos de "A" até o seu limite "N".
