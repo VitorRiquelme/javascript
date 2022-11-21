953ebf83b58a5fbdfd449ff22bdf0324e09e9ae0
VitorRiquelme <vitoriquelme1@gmail.com>
2022-11-21 18:49:11 -0300

    Update index.js

--- exdios/index.js
+++ exdios/index.js
@@ -2,5 +2,7 @@
 //javascript output
 //Usar o documnet.write depois de o documneto html já ter sido carregado ele ira sobrepor todo o elemento
 
+alert("Iae gayzao pae kkk")
+
 document.write("oláaa")
 
