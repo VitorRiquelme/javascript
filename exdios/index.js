605af85f1df151311bb498887458076f809d271d
VitorRiquelme <vitoriquelme1@gmail.com>
2022-11-21 19:43:36 -0300

    Update index.js

--- exdios/index.js
+++ exdios/index.js
@@ -1,10 +1,11 @@
 
 //javascript output
 //Usar o documnet.write depois de o documneto html já ter sido carregado ele ira sobrepor todo o elemento
+//Nomes devem começar com letras $ ou _ 
+//variaves são case sensetive entãi A é diferente de a 
+let z,x,y;
+x=10;
+y=20;
+z=x+y;
 
-let z,x,y
-x=10
-y=20
-z=x+y
-
-document.getElementById("demo").innerHTML = "o valor da soma de x + y é "+ z + "."
+document.getElementById("demo").innerHTML = "o valor da soma de x + y é "+ z + ".";
