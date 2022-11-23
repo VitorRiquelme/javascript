b30372c66295a6160bd47ffb4714cc67a4fd28f4
VitorRiquelme <vitoriquelme1@gmail.com>
2022-11-22 23:17:53 -0300

    excercicios de javascript

--- exdios/index.js
+++ exdios/index.js
@@ -3,9 +3,11 @@
 //Usar o documnet.write depois de o documneto html já ter sido carregado ele ira sobrepor todo o elemento
 //Nomes devem começar com letras $ ou _ 
 //variaves são case sensetive entãi A é diferente de a 
-let z,x,y;
+/*let z,x,y;
 x=10;
 y=20;
 z=x+y;
 
-document.getElementById("demo").innerHTML = "o valor da soma de x + y é "+ z + ".";
+document.getElementById("demo").innerHTML = "o valor da soma de x + y é "+ z + ".";*/
+
+
