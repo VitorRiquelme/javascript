28cc36afefebd79329d00398429ae795364fe665
VitorRiquelme <vitoriquelme1@gmail.com>
2022-12-05 20:17:03 -0300

    Update index.js

--- exdios/index.js
+++ exdios/index.js
@@ -96,7 +96,7 @@ correto
 };
 //nome é a propriedade e vitor é o valor da propriedade
 //Metodo de um objeto é uma função amarzenada como um valor de uma propriedade
- console.log(pessoa.nome, pessoa.sobrenome)*/
+ console.log(pessoa.nome, pessoa.sobrenome)
 
  const pessoa = {
     nome:"Vitor",
@@ -107,7 +107,7 @@ correto
    }
   };
 
-  document.getElementById("demo").innerHTML = pessoa.nomecompleto();
+  document.getElementById("demo").innerHTML = pessoa.nomecompleto(); */
 
 
 
